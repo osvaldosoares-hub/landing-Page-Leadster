1e88c720481428d860d15e1ab6eeed9d7bca0e07
osvaldosoares-hub <osvaldo.jrjuniorlinhares@gmail.com>
2023-07-06 09:16:38 -0300

    terminando a pag e o responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <div className="section">
           <button>WEBINARS EXCLUSIVO</button>
           <h2>Menos Conversinha,</h2>
-          <h1>Mais Conversão</h1>
-          <img src='../img/asset-header.png' alt='efeito'/>
+          <h1>Mais Conversão<img src='../img/asset-header.png' alt='efeito'/></h1>
+          
           <div className="borda"></div>
           <p>Conheça as estratégias que mudaram o jogo e como aplica-las no seu negócio</p>
       </div>
@@ -27,19 +27,52 @@ export default function Home() {
                   </div>
                   <div className='Coteudoone'>
                     <button>VER DEMOSTRAÇÃO</button>
-                    <img src="../img/selo_RD.png" alt="" />
+                    <img src="../img/selo_RD.png" alt="selo_RD" />
                   </div>
                   <div className='Coteudotwo'>
                     
                     <img src="../img/no-card-dark.webp" alt="" />
                     <p className='borda'>Não é necessario Cartão de Crédito</p>
-                    <img src="../img/rating.webp" alt="" />
+                    <img src="../img/rating.webp" alt="rating" />
                     <p>4.9/5 media de satisfação</p>
                   </div>
             </div>
         </div>
-        <div className='sectionTwo'>
-
+        <div className='divisor'>
+        <img src='../img/logo.png' alt='logo'/>
+        <p>Transformando visitantes em clientes.</p>
+        </div>
+        <div className='Footer'>
+        
+            <div className='lista'>
+              <h3>Links Principais</h3>
+              <p>Home</p>
+              <p>Ferramenta</p>
+              <p>Preços</p>
+              <p>Contato</p>
+            </div>
+            <div className='lista'>
+              <h3>Case</h3>
+              <p>Geração de leads B2B</p>
+              <p>Geração de leads em Software</p>
+              <p>Geração de leads em Imobiliaria</p>
+              <p>Cases de Sucesso</p>
+            </div>
+            <div className='lista'>
+              <h3>Materiais</h3>
+              <p>Blog</p>
+              <p>Parecerias com Agenciaas</p>
+              <p>Guia definitivo do Marketing</p>
+              <p>Materiais Gratuitos</p>
+            </div>
+            <div className='lista'>
+              <h3>Siga a Leadster</h3>
+              <img src='../img/linkedin.svg' alt='linkedin'/>
+              <img src='../img/facebook.svg' alt='facebook'/>
+              <img src='../img/instagram.svg' alt='instagram'/>
+              <p className='email'><span>E-mail:</span> osvaldo.jrjuniorlinhares@gmail.com</p>
+              <p><span>Telefone:</span> (88) 993443238</p>
+            </div>
         </div>
     </div>
     </>
